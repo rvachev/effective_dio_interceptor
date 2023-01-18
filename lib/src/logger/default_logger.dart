81df64529c45f164b2d7f5aff63b3dc3c0e1cd2c
Roman Rvachev <romandauria@gmail.com>
2023-01-18 20:22:17 +0600

    [+] Changed log message name

--- lib/src/logger/default_logger.dart
+++ lib/src/logger/default_logger.dart
@@ -3,21 +3,23 @@ import 'dart:developer';
 import '../models/log_message.dart';
 import 'logger.dart';
 
+const _logName = 'HTTP Logger';
+
 class DefaultLogger implements ILogger {
   const DefaultLogger();
 
   @override
   void onError(LogMessage message) {
-    log('\x1B[31m$message');
+    log('\x1B[31m$message', name: _logName);
   }
 
   @override
   void onRequest(LogMessage message) {
-    log('\x1b[33m$message');
+    log('\x1b[33m$message', name: _logName);
   }
 
   @override
   void onResponse(LogMessage message) {
-    log('\x1B[32m$message');
+    log('\x1B[32m$message', name: _logName);
   }
 }
