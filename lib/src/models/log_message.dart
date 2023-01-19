f170d1a2311641894e19079a15f86b365193f866
Roman Rvachev <romandauria@gmail.com>
2023-01-19 19:35:55 +0600

    [+] Extended log message with type getter

--- lib/src/models/log_message.dart
+++ lib/src/models/log_message.dart
@@ -2,14 +2,28 @@ import 'dart:convert';
 
 import 'package:dio/dio.dart';
 
-class LogMessage {
+const JsonEncoder _encoder = JsonEncoder.withIndent('  ');
+
+enum LogMessageType { error, request, response }
+
+abstract class LogMessage {
+  LogMessageType get type;
+
+  LogMessage();
+
+  factory LogMessage.fromError(DioError err) = _ErrorLogMessage;
+
+  factory LogMessage.fromRequest(RequestOptions options) = _RequestLogMessage;
+
+  factory LogMessage.fromResponse(Response response) = _ResponseLogMessage;
+}
+
+class _ErrorLogMessage extends LogMessage {
   final StringBuffer _buffer;
 
-  LogMessage._(this._buffer);
+  _ErrorLogMessage._(this._buffer);
 
-  static const JsonEncoder _encoder = JsonEncoder.withIndent('  ');
-
-  factory LogMessage.fromError(DioError err) {
+  factory _ErrorLogMessage(DioError err) {
     StringBuffer buffer = StringBuffer();
     buffer.writeln(
       '<-- ${err.message} ${(err.response?.requestOptions != null ? (err.response!.requestOptions.baseUrl + err.response!.requestOptions.path) : 'URL')}',
@@ -18,11 +32,23 @@ class LogMessage {
         ? _encoder.convert(err.response!.data)
         : 'Unknown Error';
     buffer.writeln(errorMessage);
-    buffer.writeln('<-- End error');
-    return LogMessage._(buffer);
+    buffer.write('<-- End error');
+    return _ErrorLogMessage._(buffer);
   }
 
-  factory LogMessage.fromRequest(RequestOptions options) {
+  @override
+  LogMessageType get type => LogMessageType.error;
+
+  @override
+  String toString() => _buffer.toString();
+}
+
+class _RequestLogMessage extends LogMessage {
+  final StringBuffer _buffer;
+
+  _RequestLogMessage._(this._buffer);
+
+  factory _RequestLogMessage(RequestOptions options) {
     StringBuffer buffer = StringBuffer();
     buffer.writeln(
       '--> ${options.method.toUpperCase()} ${options.baseUrl}${options.path}',
@@ -41,13 +67,25 @@ class LogMessage {
         buffer.writeln(_encoder.convert(options.data));
       }
     }
-    buffer.writeln(
+    buffer.write(
       '--> END ${options.method.toUpperCase()}',
     );
-    return LogMessage._(buffer);
+    return _RequestLogMessage._(buffer);
   }
 
-  factory LogMessage.fromResponse(Response response) {
+  @override
+  LogMessageType get type => LogMessageType.request;
+
+  @override
+  String toString() => _buffer.toString();
+}
+
+class _ResponseLogMessage extends LogMessage {
+  final StringBuffer _buffer;
+
+  _ResponseLogMessage._(this._buffer);
+
+  factory _ResponseLogMessage(Response response) {
     StringBuffer buffer = StringBuffer();
     buffer.writeln(
         '<-- ${response.statusCode} ${response.requestOptions.baseUrl + response.requestOptions.path}');
@@ -55,9 +93,12 @@ class LogMessage {
     response.headers.forEach((k, v) => buffer.writeln('$k: $v'));
     buffer.writeln('--Response--');
     buffer.writeln(_encoder.convert(response.data));
-    buffer.writeln('<-- END HTTP');
-    return LogMessage._(buffer);
+    buffer.write('<-- END HTTP');
+    return _ResponseLogMessage._(buffer);
   }
+
+  @override
+  LogMessageType get type => LogMessageType.response;
 
   @override
   String toString() => _buffer.toString();
